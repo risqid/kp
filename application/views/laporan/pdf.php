c7ced6b40f3f3c058df3664feadb565e9f1a2b83
risqid <risqidharmawan@gmail.com>
2021-01-31 19:08:33 +0700

    modified exported pdf file name

--- application/views/laporan/pdf.php
+++ application/views/laporan/pdf.php
@@ -69,4 +69,4 @@ ob_start();
     $content = ob_get_contents();
 ob_end_clean();
 $obj_pdf->writeHTML($content, true, false, true, false, '');
-$obj_pdf->Output('output.pdf', 'I');
+$obj_pdf->Output('laporan keuangan tahun '.$tahun.'.pdf', 'I');
